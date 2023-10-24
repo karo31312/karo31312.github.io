5bc78eb51455517115a58fb766253a97f8c49502
karo31312 <148896831+karo31312@users.noreply.github.com>
2023-10-24 21:08:13 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,147 @@
+// let newDatee = new Date()
+// let year = document.getElementById("inp1")
+// let mount = document.getElementById("inp2")
+// let day = document.getElementById("inp3")
+// let h1 = document.getElementById("h1")
+// let btn = document.getElementById("btn")
+
+
+
+
+
+// btn.addEventListener("click", () =>{
+    
+//     if (typeof +year.value == "number" && typeof +mount.value == "number" && typeof +day.value == "number" ) {
+//         let newYear = new Date (+year.value, +mount.value - 1, +day.value);
+
+
+//         let currentDate  = new Date( );
+//         let interval = newYear - currentDate; 
+        
+
+//     // console.log(Math.floor(interval / 1000 / 60 / 60 / 24)); 
+//     // console.log(Math.floor(interval % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))); 
+//     // console.log(Math.floor(interval % (1000 * 60 * 60) / (1000 * 60))); 
+//     // console.log(Math.floor(interval % (1000 * 60) / (1000)));
+
+
+//         if (interval >= 0) {
+//             h1.innerHTML = `${Math.floor(interval / 1000 / 60 / 60 / 24)}day ${Math.floor(interval % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))}hour ${Math.floor(interval % (1000 * 60) / (1000))}second `
+            
+//         }else{
+//             h1.innerHTML = "inncorrect"
+//         }
+
+
+
+//     }
+// })
+
+
+
+
+
+
+
+
+// let getYear = +prompt("get year dear user ") 
+// let newYear = new Date(getYear, 8 , 7 ); 
+// let currentDate  = new Date( ); 
+// let interval = newYear - currentDate; 
+ 
+// console.log(newYear); 
+// console.log(currentDate); 
+ 
+// console.log(interval); 
+// console.log(Math.floor(interval / 1000 / 60 / 60 / 24)); 
+// console.log(Math.floor(interval % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))); 
+// console.log(Math.floor(interval % (1000 * 60 * 60) / (1000 * 60))); 
+// console.log(Math.floor(interval % (1000 * 60) / (1000)));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
